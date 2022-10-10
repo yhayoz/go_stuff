bbc2d8af3a1e6d19cc10d02825dc9c21f36e96de
Yannic Hayoz <yannic.hayoz@rega-sense.ch>
2022-10-10 21:25:39 +0200

    some improvements

--- main.go
+++ main.go
@@ -1,80 +1,56 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strings"
 )
 
-// set global Vars to use in diffrent functions
-
 func main() {
+
 	// Declare some default VARs
+
+	// Some cli arguments
+	var structure = flag.String("s", "simple", "Structure simple or complex")
+	var hlq = flag.String("h", "", "HLQ")
+	var app = flag.String("n", "", "App Name")
+	var stages = flag.String("e", "", "Stages to create")
+	flag.Parse()
+
 	var default_stages = "dev,test,prod"
 	var default_structure = "simple"
 
-	// Ask Some Questions for the Project
-	fmt.Println("/(default structure:", default_structure, ") customer project structure type to create? [simple|complex]: ")
-	var structure string
-	fmt.Scanln(&structure)
-	if structure == "" {
-		structure = default_structure
+	if *structure == "" {
+		structure = &default_structure
 	}
 
-	fmt.Println("HLQ:")
-	var hlq string
-	fmt.Scanln(&hlq)
-
-	fmt.Println("Name of App:")
-	var app string
-	fmt.Scanln(&app)
-
-	fmt.Println("(default stages:", default_stages, ") stages:")
-	var stages string
-	fmt.Scanln(&stages)
-	if stages == "" {
-		stages = default_stages
+	if *stages == "" {
+		stages = &default_stages
 	}
-	arr := strings.Split(stages, ",")
-	fmt.Println(arr)
-
+	var arr = strings.Split(*stages, ",")
 	fmt.Println(`We Create following Repo:`, "\n",
-		`Structure:`, structure, "\n",
-		`HLQ:`, hlq, "\n",
-		`App Name:`, app, "\n",
+		`Structure:`, *structure, "\n",
+		`HLQ:`, *hlq, "\n",
+		`App Name:`, *app, "\n",
 		`Stages:`, arr)
-	create_structure(app, arr, hlq, structure)
+
+	create_structure(arr, hlq, app, structure)
 }
 
-func contains(arr []string, str string) bool {
-
-	for _, v := range arr {
-		if v == str {
-
-			fmt.Println("Exists")
-			return true
-		}
-	}
-
-	fmt.Println("does not exist")
-	return true
-}
-
-func create_structure(app string, arr []string, hlq string, structure string) {
-	contains(arr, "test")
-	if structure == "simple" {
+func create_structure(arr []string, hlq *string, app *string, structure *string) {
+	if *structure == "simple" {
 		// First create base Directorie
-		os.MkdirAll(fmt.Sprintf("../deploy/stages/base/%s", app), os.ModePerm)
+		os.MkdirAll(fmt.Sprintf("../deploy/stages/base/%s", *app), os.ModePerm)
 
 		for i := 0; i < len(arr); i++ {
-			os.MkdirAll(fmt.Sprintf("../deploy/stages/%s", arr[i]), os.ModePerm)
+			os.MkdirAll(fmt.Sprintf("../deploy/stages/%s/%s", arr[i], *app), os.ModePerm)
 		}
 	}
 
-	if structure == "complex" {
+	if *structure == "complex" {
 		// First create all Directories
-		fmt.Println("hello", app)
-		os.MkdirAll(fmt.Sprintf("../deploy/base/%s", app), os.ModePerm)
+		os.MkdirAll(fmt.Sprintf("../deploy/base/%s", *app), os.ModePerm)
 	}
 
 }
